1867799a96f2fb1d3bdb6422b9b80c8ca2cdec7e
dalibobo <153897761@qq.com>
2016-06-28 09:50:15 +0800

    修改文档

--- doc/main.js
+++ doc/main.js
@@ -13,33 +13,34 @@ $(document).ready(function() {
 	}, 100);
 });
 
-// 初始化impex
-function initImpex() {
-	page = impex.render(document.body, {
-		proRows: [
-			{name: "id", type: "String", descr: "id属性，通过该属性可获取组件的数据模型", default: ""}
-		]
-	});
-}
-
 impex.component("x-doc-table", {
 	$template: '<table class="doc-table">\
-				<tbody><tr>\
+				<tbody>\
+				<tr x-if="item.type">\
 				<th><strong>名称</strong></th>\
 				<th><strong>类型</strong></th>\
 				<th><strong>描述</strong></th>\
 				<th><strong>默认值</strong></th>\
 				</tr>\
+				<tr x-if="!item.type">\
+				<th><strong>名称</strong></th>\
+				<th><strong>参数</strong></th>\
+				<th><strong>描述</strong></th>\
+				</tr>\
 				<tr x-each="datas as d">\
 				<td>{{d.name}}</td>\
-				<td>{{d.type}}</td>\
-				<td>{{d.descr}}</td>\
-				<td>{{d.default}}</td>\
+				<td x-if="item.type">{{d.type}}</td>\
+				<td x-if="!item.type">{{d.parameter}}</td>\
+				<td>{{# d.descr}}</td>\
+				<td x-if="item.type">{{d.default}}</td>\
 				</tr>\
 				</table>',
 	onInit: function() {
 		if (this.rows) {
-			this.datas = this.closest(this.rows)[this.rows];
+			this.datas = this.closest(this.rows)[this.rows] || [];
+			if (this.datas.length > 0) {
+				this.item = this.datas[0];
+			}
 		}
 	}
 });
