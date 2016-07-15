c13e5224207642415f55cf8c8d7826fbf6df1fae
dalibobo <153897761@qq.com>
2016-07-15 19:01:21 +0800

    修改impex-select

--- build/impexui.all.js
+++ build/impexui.all.js
@@ -369,6 +369,81 @@ var Tip = {
 }
 
 /**
+ * 校验指令
+ * <br/>使用方式：<input x-validate="required,isint:'+',length:2:6}"></input>
+ * true:表示需要校验，false:不做校验
+ * msg:提示消息
+ * 例子：required,  需要做required校验，
+ * isint:'+'
+ */
+impex.directive('validate',{
+	checkModel:"",
+	enabled: true,
+	do: function() {
+		if (!this.enabled) return true;
+		var obj = this.checkModel.split(",");
+		var objValue = this.view.el.value;
+		var reType = true;
+		var msg = "验证通过！";
+		var v;
+		var validateResult = {result: true};
+		for ( var p in obj ){ // 方法 
+			v = obj[p];
+			var lr = v.split(":");
+			if(!lr[0] || lr[0] === 'msg'){
+				continue;
+			}
+			
+			var rs = {type:true,msg:""};
+			if(undefined === impex.validate[lr[0]]){
+				rs.type = false;
+				rs.msg = lr[0]+"函数不存在!";
+			}else{
+				rs = impex.validate[lr[0]](objValue,lr.splice(1));
+			}
+			
+			if(!rs.type){
+				reType = false;
+				validateResult.result = false;
+				validateResult.msg = obj.msg || rs.msg ;
+				break;
+			}
+		}
+		//this.emit("impex.validate.result",  validateResult);
+		
+		if(!validateResult.result){
+			var el = $(this.view.el);
+			var of = el.offset() ;
+			Tip.show("tip-"+this.view.el.id,{
+				left:of.left+of.width,
+				top:of.top,
+				right:0,
+				bottom:0,
+				dir:'right',
+				message:validateResult.msg
+			});
+		}else{
+			$("#tip-"+this.view.el.id).remove();
+		}
+		this.emit("impex.validate.result",  validateResult);
+		return reType;
+	},
+	onInit: function() {
+		var that = this;
+		$(this.view.el).on("input", function() {
+			that.emit("validate.fire", that.do());
+		});
+		this.checkModel = this.value + "";
+		if (undefined == this.data.xDisabled) {
+			this.enabled = true;
+		}else{
+			this.enabled = this.data.xDisabled;
+		}
+	}
+		
+});
+
+/**
  * 表单提交
  */
 impex.directive('submit',{
